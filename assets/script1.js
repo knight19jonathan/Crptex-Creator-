3a27e0c92a292f3a4879ead564ac3045209970dd
Magnus Did Nothing Wrong <knight19jonathan@gmail.com>
2022-06-27 16:57:58 -0700

    fixing password push to text box

--- assets/script1.js
+++ assets/script1.js
@@ -1,3 +1,4 @@
+const realPassword = [];
 var password;
 var length;
 var password_characters = "";
@@ -5,14 +6,12 @@ var upper_case = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ';
 var lower_case = 'abcdefghijklmnopqrstuvwxyz';
 var special_set = '~!@#$%^&*()_+<>?,./{}][|';
 var num_set = '124567890'
-
-
-
 var generateBtn = document.querySelector("#generate");
-generateBtn.addEventListener("click", function(){ getPassword(writePassword)}); 
 
 
-function getPassword(callback) {
+generateBtn.addEventListener("click", function(){writePassword()});
+
+function getPassword() {
 
     let parameters = {
         pwlength: Number(),
@@ -24,83 +23,103 @@ function getPassword(callback) {
 
     var temporaryPassword = '';
 
-    parameters.pwlength = +prompt("Pick a number of characters between 8 and 128 for your password.") // prompt to ask password length
+    parameters.pwlength = +prompt("Pick a number of characters between 8 and 128 for your password.") 
 
     if (parameters.pwlength<8 || 0) {
         window.alert("Must be more than 8 characters.")
-        location.reload();
+        return;
     } else if (parameters.pwlength>128) {
         window.alert("Cannot exceed 128 characters.")
-        window.reload();
+        return;
     } else (parameters.pwlength>=8 && parameters.pwlength<=128)
-    // save varaiale of password length 
+
     const characterTypesInPassword = parameters.pwlength;
 
-    // confirm to ask for Capital Letters
+
     parameters.uPPErChars = confirm("Include CAPITAL letters in your password?");
     if (parameters.uPPErChars == true) {
-       // console.log(passwordCharacters = passwordCharacters += upper_case);
+
         temporaryPassword = temporaryPassword.concat(upper_case);
-        // window.alert("Included CAPITAL letters.") 
+
     } else {
-        // window.alert("Password will not include CAPITAL letters.")
+        
     }
 
-    // confirm to ask for lower case letters
+
     parameters.lowerChars = confirm("Include lower case letters?") 
     if (parameters.lowerChars == true) {
-       // console.log(passwordCharacters = passwordCharacters += lowerChars);
+ 
         temporaryPassword = temporaryPassword.concat(lower_case);
-        // window.alert("Included lower case letters.")
+       
     } else {
-        // window.alert("Password will not include lower case letters.")
+        
     }
 
-    // confirm to ask for Special characters
     parameters.sPecChars = confirm("Include Special characters?")
     if (parameters.sPecChars == true) {
-       // console.log(passwordCharacters = passwordCharacters += sPecChars);
+
         temporaryPassword = temporaryPassword.concat(special_set);
-        // window.alert("Included Special characters.")
+
     } else {
-        // window.alert("Excluded Special characters.")
+        
     } 
 
-    // confirm to ask for Numbers
     parameters.nUmChars = confirm("Include numbers?")
     if (parameters.nUmChars == true) {
-      //  console.log(passwordCharacters = passwordCharacters += nUmChars);
+
         temporaryPassword = temporaryPassword.concat(num_set);
-        // window.alert("Included numbers.")
+    
     } else {
-        // window.alert("Excluded numbers.")
+         
     }
     
-    console.log(temporaryPassword);
-
-   
-   
     
-    const realPassword = [];
+
+
     for (let i = 0; i < parameters.pwlength; i++) {
         lucky_num = Math.floor(Math.random() * ((temporaryPassword.length-1 - 0)+0));
         realPassword[i]= temporaryPassword[lucky_num];
-        console.log(realPassword);
+        realPassword[i]=password;  
+        console.log(password)
+
+        // return password;
+
+
+        
     }
-    callback();
     
-    
+
 }
+
+function writePassword() {
+    password = getPassword();
+    passwordText = document.querySelector("#password")
+    passwordText.value = password
+} 
+console.log.typeof(password)
+
+
+
+
+// function writePassword() {
+//     var password = getPassword();
+//     var passwordText = document.querySelector("#password")
+//     passwordText.value = password;
+// }
+
+
+
+
+
+
+
+
+
+
+
 
 
 // generateBtn.addEventListener("click", function(){ writePassword()}); 
-
-function writePassword() {
-    var password = getPassword();
-    var passwordText = document.querySelector("#password")
-    passwordText.value = password;
-}
-
 
 
 
